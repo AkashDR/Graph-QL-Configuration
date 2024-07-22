bfc7ba7460ebb89028e3d81db142db839738acaf
Akash <akashdandur@gmail.com>
2024-07-22 19:30:21 +0530

    Final

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -23,16 +23,15 @@ function App() {
   return (
     <div>
       <h1 className="Heading">Students List</h1>
-      <button
-        className="Button-class"
-        onClick={handleClick}
-      >
+      <button className="Button-class" onClick={handleClick}>
         Get Students Data
       </button>
       <div>
         {data?.students?.map((student) => (
           <ul className="Student-data" key={student?.id}>
-            <h4>Student Name: {student?.firstName + student?.lastName}</h4>
+            <h4>
+              Student Name: {student?.firstName + " " + student?.lastName}
+            </h4>
             <h4>Student ID: {student?.id}</h4>
             <h4>Student Email: {student?.email}</h4>
             <hr />
